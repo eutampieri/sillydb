8bbef0d2d6c83161e937b58737d839e2755da2fd
Eugenio Tampieri <eugenio@eutampieri.eu>
2025-02-19 19:13:30 +0100

    Query mapping

--- src/postgres/mod.rs
+++ src/postgres/mod.rs
@@ -1,5 +1,45 @@
 use super::Database;
 
+fn map_query(q: &str) -> String {
+    struct Ctx {
+        idx: usize,
+        result: String,
+    }
+
+    impl Ctx {
+        fn append(self, c: String) -> Self {
+            Self {
+                idx: self.idx,
+                result: self.result + &c,
+            }
+        }
+        fn inc(self) -> Self {
+            Self {
+                idx: self.idx + 1,
+                result: self.result,
+            }
+        }
+    }
+
+    q.chars()
+        .into_iter()
+        .fold(
+            Ctx {
+                idx: 0,
+                result: "".to_owned(),
+            },
+            |acc, x| match x {
+                '?' => {
+                    let a = acc.inc();
+                    let idx = a.idx;
+                    a.append(format!("${}", idx))
+                }
+                _ => acc.append(x.to_string()),
+            },
+        )
+        .result
+}
+
 impl Database for postgres::Client {
     type Error = postgres::Error;
 
@@ -15,5 +55,18 @@ impl Database for postgres::Client {
         params: &[crate::SqlValue],
     ) -> Result<Vec<std::collections::HashMap<String, crate::SqlValue>>, Self::Error> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn query_conversion_changes_question_marks_to_dollar_index() {
+        let query = "SELECT * FROM test WHERE a = ?;";
+        let expected = "SELECT * FROM test WHERE a = $1;";
+        let actual = map_query(query);
+        assert_eq!(expected, actual);
     }
 }
